73f89276198140ac4ccf6f96ecdf8e905045286f
Chase <Chase>
2016-01-11 16:38:45 -0800

    Snake Shell Game Final

--- snake.js
+++ snake.js
@@ -1,21 +1,25 @@
 //direction for both snakes
 var direction = 'noDirection';
 var direction2 = 'noDirection';
+//shedded skin for both snakes
+var snakeSkin = [];
+var snake2Skin = [];
 
 var maxWidthGlobal = document.getElementById('canvas').width;
 var cellWidth = 15;
 var farCorner = maxWidthGlobal/cellWidth;
 var snake = [
-    {x: 2, y: 0, color: "#0000ff"},
-    {x: 1, y: 0, color: "#0000ff"},
-    {x: 0, y: 0, color: "#0000ff"}
+    {x: 2, y: 0, color: "#F6071E"},
+    {x: 1, y: 0, color: "#F6071E"},
+    {x: 0, y: 0, color: "#F6071E"}
 ];
 
 var snake2 = [
-    {x: 27, y: 29, color: "#ff0000"},
-    {x: 28, y: 29, color: "#ff0000"},
-    {x: 29, y: 29, color: "#ff0000"}
+    {x: 27, y: 29, color: "#05C12F"},
+    {x: 28, y: 29, color: "#05C12F"},
+    {x: 29, y: 29, color: "#05C12F"}
 ];
+var paused = false;
 
 
 $(document).ready(function() {
@@ -30,17 +34,15 @@ $(document).ready(function() {
 
     var start = null;
     //speed of the game in miliseconds
-    var beginTime = 200;
+    var beginTime = 100;
     //interval name (not active at first)
     var snakeMove = 'notYet';
     //previous direction for both snakes
     var prevDir = 'noDirection';
     var prevDir2 = 'noDirection';
 
-    //arrays containing the food item array and each snake's poop
+    //arrays containing the food item array and each snake's Skin
     var myFood = [];
-    var snakePoop = [];
-    var snakePoop2 = [];
 
     function resetGame() {
         clearInterval(snakeMove);
@@ -51,17 +53,17 @@ $(document).ready(function() {
         start = null;
         direction = 'noDirection';
         direction2 = 'noDirection';
-        snakePoop = [];
-        snakePoop2 = [];
+        snakeSkin = [];
+        snake2Skin = [];
         snake = [
-            {x: 2, y: 0, color: "#0000ff"},
-            {x: 1, y: 0, color: "#0000ff"},
-            {x: 0, y: 0, color: "#0000ff"}
+            {x: 2, y: 0, color: "#F6071E"},
+            {x: 1, y: 0, color: "#F6071E"},
+            {x: 0, y: 0, color: "#F6071E"}
         ];
         snake2 = [
-            {x: 27, y: 29, color: "#ff0000"},
-            {x: 28, y: 29, color: "#ff0000"},
-            {x: 29, y: 29, color: "#ff0000"}
+            {x: 27, y: 29, color: "#05C12F"},
+            {x: 28, y: 29, color: "#05C12F"},
+            {x: 29, y: 29, color: "#05C12F"}
         ];
         createBlock(myFood, false, false);
         paintArray(myFood);
@@ -76,8 +78,8 @@ $(document).ready(function() {
         snakeMove = 'notYet';
         start = null;
         createBlock(myFood, false, false);
-        paintArray(snakePoop);
-        paintArray(snakePoop2);
+        paintArray(snakeSkin);
+        paintArray(snake2Skin);
         paintArray(myFood);
         paintArray(snake);
         paintArray(snake2);
@@ -85,10 +87,8 @@ $(document).ready(function() {
     }
 
     //sets up the canvas for the first time
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = '#FFC107';
     ctx.fillRect(0, 0, w, h);
-    ctx.strokeStyle = "black";
-    ctx.strokeRect(0,0,w,h);
 
     //creates random number (put in a parameter if the limit doesn't apply to the board width/height)
     function randomNumber(optionalLimit) {
@@ -102,7 +102,7 @@ $(document).ready(function() {
         var square = {
             x: x !== false ? x : randomNumber(),
             y: y !== false ? y : randomNumber(),
-            color: "green"
+            color: "white"
         };
         foodVar[0] = square;
         return foodVar;
@@ -128,20 +128,24 @@ $(document).ready(function() {
 
     //resets the canvas each time to so that things can appear to move
     function resetCanvas() {
-        ctx.fillStyle = "white";
+        ctx.fillStyle = '#FFC107';
         ctx.fillRect(0, 0, w, h);
-        ctx.strokeStyle = 'black';
-        ctx.strokeRect(0,0, w, h);
     }
 
     //changes the global direction variable
     $(document).keydown(function(e) {
+        if (paused) {
+            return;
+        }
         prevDir = direction; //important for the snake slither motion
         prevDir2 = direction2;
         var key = e.which;
         switch (key) {
             case 37: //left arrow
                 direction = start === null? 'right' : prevDir === 'right' ? 'right' : 'left';
+                if (prevDir === direction && snake.length > 3) {
+                    shedSkin("snake");
+                }
                 if (snakeMove === 'notYet') {
                     checkNull("direction2", "left");
                     snakeMove = setInterval(moveSnake, beginTime);
@@ -150,6 +154,9 @@ $(document).ready(function() {
                 break;
             case 39: //right arrow
                 direction = prevDir === 'left' ? 'left' : 'right';
+                if (prevDir === direction && snake.length > 3) {
+                    shedSkin("snake");
+                }
                 if (snakeMove === 'notYet') {
                     checkNull("direction2", "left");
                     snakeMove = setInterval(moveSnake, beginTime);
@@ -158,6 +165,9 @@ $(document).ready(function() {
                 break;
             case 38: //up arrow
                 direction = start === null? 'right' : prevDir === 'down' ? 'down' : 'up';
+                if (prevDir === direction && snake.length > 3) {
+                    shedSkin("snake");
+                }
                 if (snakeMove === 'notYet') {
                     checkNull("direction2", "left");
                     snakeMove = setInterval(moveSnake, beginTime);
@@ -166,6 +176,9 @@ $(document).ready(function() {
                 break;
             case 40: //down arrow
                 direction = start === null? 'right' : prevDir === 'up' ? 'up' : 'down';
+                if (prevDir === direction && snake.length > 3) {
+                    shedSkin("snake");
+                }
                 if (snakeMove === 'notYet') {
                     checkNull("direction2", "left");
                     snakeMove = setInterval(moveSnake, beginTime);
@@ -174,6 +187,9 @@ $(document).ready(function() {
                 break;
             case 65: //character a (left for player two)
                 direction2 = prevDir2 === 'right' ? 'right' : 'left';
+                if (prevDir2 === direction2 && snake2.length > 3) {
+                    shedSkin("snake2");
+                }
                 if (snakeMove === 'notYet') {
                     checkNull("direction", "right");
                     snakeMove = setInterval(moveSnake, beginTime);
@@ -182,6 +198,9 @@ $(document).ready(function() {
                 break;
             case 87: //character w (up for player two)
                 direction2 = start === null ? 'left' : prevDir2 === 'down' ? 'down' : 'up';
+                if (prevDir2 === direction2 && snake2.length > 3) {
+                    shedSkin("snake2");
+                }
                 if (snakeMove === 'notYet') {
                     checkNull("direction", "right");
                     snakeMove = setInterval(moveSnake, beginTime);
@@ -190,6 +209,9 @@ $(document).ready(function() {
                 break;
             case 83: //character s (down for player two)
                 direction2 = start === null ? 'left' : prevDir2 === 'up' ? 'up' : 'down';
+                if (prevDir2 === direction2 && snake2.length > 3) {
+                    shedSkin("snake2");
+                }
                 if (snakeMove === 'notYet') {
                     checkNull("direction", "right");
                     snakeMove = setInterval(moveSnake, beginTime);
@@ -198,6 +220,9 @@ $(document).ready(function() {
                 break;
             case 68: //character d (right for player two)
                 direction2 = start === null ? 'left' : prevDir2 === 'left' ? 'left' : 'right';
+                if (prevDir2 === direction2 && snake2.length > 3) {
+                    shedSkin("snake2");
+                }
                 if (snakeMove === 'notYet') {
                     checkNull("direction", "right");
                     snakeMove = setInterval(moveSnake, beginTime);
@@ -242,11 +267,13 @@ $(document).ready(function() {
             snakeSlither("snake2", 'down');
         }
         resetCanvas();
-        paintArray(snakePoop);
-        paintArray(snakePoop2);
+        paintArray(snakeSkin);
+        paintArray(snake2Skin);
         paintArray(snake2);
         paintArray(snake);
         paintArray(myFood);
+        hitSkin("snake2");
+        hitSkin("snake");
     }
 
     //this function makes the snake-like movement to create the slither motion
@@ -273,7 +300,7 @@ $(document).ready(function() {
 
             //puts the right color after the base snake
             if (snake.length > 3) {
-                snake[3]["color"] = "#7f7fff";
+                snake[3]["color"] = "#F6071E";
             }
             checkForOffBoard("snake");
             eatFood(direction, "snake");
@@ -300,15 +327,15 @@ $(document).ready(function() {
 
             //puts the right color after the base snake
             if (snake2.length > 3) {
-                snake2[3]["color"] = "#ff7f7f";
+                snake2[3]["color"] = "#05C12F";
             }
             checkForOffBoard("snake2");
             eatFood(direction, "snake2");
             crashCheck("snake2");
         }
         resetCanvas();
-        paintArray(snakePoop);
-        paintArray(snakePoop2);
+        paintArray(snakeSkin);
+        paintArray(snake2Skin);
         paintArray(snake2);
         paintArray(snake);
         paintArray(myFood);
@@ -317,20 +344,20 @@ $(document).ready(function() {
     function eatFood(dir, whichSnake) {
         var foodLoc = myFood[0];
         var lastSnakeBlock = whichSnake === 'snake' ? snake[snake.length - 1] : snake2[snake2.length - 1];
-        var poopColor = whichSnake === 'snake' ? '#7f7fff' : '#ff7f7f';
+        var SkinColor = whichSnake === 'snake' ? '#F6071E' : '#05C12F';
         window[whichSnake].forEach(function(obj) {
             if (foodLoc.x === obj.x && foodLoc.y === obj.y) {
                 if (dir === 'up') {
-                    window[whichSnake].push({x: lastSnakeBlock.x, y: lastSnakeBlock.y, color: poopColor});
+                    window[whichSnake].push({x: lastSnakeBlock.x, y: lastSnakeBlock.y, color: SkinColor});
                 }
                 else if (dir === 'down') {
-                    window[whichSnake].push({x: lastSnakeBlock.x, y: lastSnakeBlock.y, color: poopColor});
+                    window[whichSnake].push({x: lastSnakeBlock.x, y: lastSnakeBlock.y, color: SkinColor});
                 }
                 else if (dir === 'right') {
-                    window[whichSnake].push({x: lastSnakeBlock.x, y: lastSnakeBlock.y, color: poopColor});
+                    window[whichSnake].push({x: lastSnakeBlock.x, y: lastSnakeBlock.y, color: SkinColor});
                 }
                 else {
-                    window[whichSnake].push({x: lastSnakeBlock.x, y: lastSnakeBlock.y, color: poopColor});
+                    window[whichSnake].push({x: lastSnakeBlock.x, y: lastSnakeBlock.y, color: SkinColor});
                 }
                 createBlock(myFood, false, false);
                 paintArray(myFood);
@@ -365,22 +392,77 @@ $(document).ready(function() {
     //can't crash into itself with this function
     function crashCheck(whichSnake) {
         var snakeLength = whichSnake === 'snake' ? snake.length : snake2.length;
-        for (var x = 0; x < snakeLength; x++) {
-            var same = 0;
-            var obj1 = window[whichSnake][x];
-            for (var i = 0; i < snakeLength; i++) {
-                if (obj1.x === window[whichSnake][i].x && obj1.y === window[whichSnake][i].y) {
-                    same++;
+        var head = window[whichSnake][0];
+        var same = 0;
+        for (var i = 0; i < snakeLength; i++) {
+            if (head.x === window[whichSnake][i].x && head.y === window[whichSnake][i].y) {
+                same++;
+            }
+            if (same > 1 && i !== snakeLength - 1) {
+                if (whichSnake === 'snake') {
+                    swal({
+                        title: "Green snake wins!",
+                        text: "Green snake crashed into itself.",
+                        type: "success",
+                        showConfirmButton: true,
+                        showCancelButton: false,
+                        closeOnConfirm: true,
+                        confirmButtonText: "Play Again"},
+                        function(isConfirm) {
+                            if (isConfirm) {
+                                resetGame();
+                            }
+                        }
+                     )
                 }
-                if (same > 1 && i !== snakeLength - 1) {
-                    if (whichSnake === 'snake') {
-                        alert("Congrats player 2, you won the game!");
-                    }
-                    else {
-                        alert("Congrats player 1, you won the game!");
-                    }
-                    return resetGame();
+                else {
+                    swal({
+                        title: "Red Snake wins!",
+                        text: "Green snake crashed into itself.",
+                        type: "success",
+                        showConfirmButton: true,
+                        showCancelButton: false,
+                        closeOnConfirm: true,
+                        confirmButtonText: "Play Again"},
+                        function(isConfirm) {
+                            if (isConfirm) {
+                                resetGame();
+                            }
+                        }
+                     )
                 }
+            }
+        }
+    }
+
+    function shedSkin(whichSnake) {
+        var shedded = window[whichSnake].pop();
+        window[whichSnake + "Skin"].push(shedded);
+        paintArray(window[whichSnake]);
+    }
+    function hitSkin(whichSnake) {
+        var player = whichSnake === 'snake' ? 'Green snake' : 'Red Snake';
+        var player2 = whichSnake === 'snake' ? 'Red Snake' : 'Green snake';
+        var skin = whichSnake === "snake" ? window["snake2Skin"] : window["snakeSkin"];
+        var snake = window[whichSnake];
+        var snakeLength = snake.length;
+        var skinLength = skin.length;
+        for (var i = 0; i < skinLength; i++) {
+            if (snake[0].x === skin[i].x && snake[0].y === skin[i].y) {
+                swal({
+                    title: player + " wins!",
+                    text: player2 + " crashed into your skin.",
+                    type: "success",
+                    showConfirmButton: true,
+                    showCancelButton: false,
+                    closeOnConfirm: true,
+                    confirmButtonText: "Play Again"},
+                    function(isConfirm) {
+                        if (isConfirm) {
+                            resetGame();
+                        }
+                    }
+                 )
             }
         }
     }
